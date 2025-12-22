9c9e3ecbf5c298174cae17cc7d4c3f849e08b6fd
hanseonghyeonnam <120729157+hanseonghyeonnam@users.noreply.github.com>
2025-12-23 03:47:50 +0900

    Update url.js

--- _/lib/url.js
+++ _/lib/url.js
@@ -1,1 +1,12 @@
-
+// import url_getter, { VirtualDomElement, ClassLoader, CheckJqueryStatus } from "https://okmpl.kro.kr/_/lib/url.js";
+export default function url_getter () {};
+export function VirtualDomElememt() {
+  return {
+    UID: "",
+  };
+};
+export function CheckJqueryStatus() {
+  if (this["$"] || window["$"]) {
+    return true;
+  } else return false;
+};
