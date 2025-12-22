b46456ace9002251bc5d6dcd7a2eb0089aef2fbe
hanseonghyeonnam <120729157+hanseonghyeonnam@users.noreply.github.com>
2025-12-23 03:01:30 +0900

    Create ui.js

---
+++ _/ui.js
@@ -0,0 +1,36 @@
+/* Published by hanseonghyeonnam */
+
+// Loads our library :D
+import url_getter, { VirtualDomElement, ClassLoader, CheckJqueryStatus } from "https://okmpl.kro.kr/_/lib/url.js";
+
+if (!CheckJqueryStatus()) {
+  return alert("Sorry, something went wrong. Please reload the site!");
+}
+
+const mainElememt = ClassLoader("ui");
+
+export default class ui {
+  static alert(msg, {
+    tick: "3ms",
+  }) {
+    const builder = VirtualDomElement(mainElement.UID);
+    
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
