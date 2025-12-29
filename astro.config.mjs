f2bd54f01b9d5208a8b4f5c1cfc8e071fc321f82
hanseonghyeonnam <hanseonghyeonnam@gmail.com>
2025-12-29 18:15:11 +0900

    ETC

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import tsconfigPaths from "vite-tsconfig-paths";
 import db from '@astrojs/db';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db()]
+  integrations: [db()],
+  vite: {
+    plugins: [
+      tsconfigPaths()
+    ]
+  }
 });
