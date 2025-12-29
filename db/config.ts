123e12e313815d11aca24ff7b257a48ac092a5d8
hanseonghyeonnam <hanseonghyeonnam@gmail.com>
2025-12-29 18:11:41 +0900

    ETC

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,16 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column } from 'astro:db';
+
+const users = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    text: column.text(),
+    body: column.text()
+  }
+});
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: {
+    users
+  }
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,16 @@
-import { db } from 'astro:db';
+import { db, user } from 'astro:db';
 
-// https://astro.build/db/seed
-export default async function seed() {
-	// TODO
+export default async function () {
+  await db.insert(user).values([
+    {
+    	id: 1,
+    	text: "Hello",
+    	body: `
+    		Hello, I'm the site developer, And admin, And manager. This server fully free and 'unlimited' note server,
+    		But some actions (like server attack behavior, ...) will blocked.
+
+    		If not? ENJOY.
+    	`
+    }
+  ]);
 }
